872a47d4f87a5c242f38462de73fe8a5aef04cf9
Megan McHugh <megan.mchugh@gmail.com>
2022-10-07 09:14:07 -0500

    added loading saved data from local storage

--- assets/script.js
+++ assets/script.js
@@ -53,6 +53,14 @@ $(document).ready(function(){
     var interval = setInterval(hourUpdater, 60000);
     //for each hour, load saved data from local storage
     $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+    $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+    $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+    $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+    $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+    $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+    $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+    $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+    $("#hour-17 .description").val(localStorage.getItem("hour-17"));
     //continue
     //display current day on the page
     $("#currentDay").text(moment().format("dddd, MMMM Do"))
